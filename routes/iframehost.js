e3ff6458ec9807dfd431e6821d225f9765a4bfde
BAINESG203\cjohnson <carjohnson@gmail.com>
2026-05-01 12:25:27 -0400

    TEST: for deploy - bypass the requireLogin to determine if the authentication middleware is blocking access and doing a redirect

--- routes/iframehost.js
+++ routes/iframehost.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const iframehost_controller = require("../controllers/iframehostController");
 const { requireLogin } = require('../middleware/auth');
 
-router.get('/', requireLogin, iframehost_controller.index);
+// router.get('/', requireLogin, iframehost_controller.index);
+router.get('/',  iframehost_controller.index);
 
 module.exports = router;

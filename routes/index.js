32b8a0ceedce63f365f1cbf8ef04a6ab727930cb
BAINESG203\cjohnson <carjohnson@gmail.com>
2026-05-04 11:17:52 -0400

    DEBUG: The index.js was routing '/' to the login screen. Trying to redirect this to the iframehost ( it may need to be to the webquiz route?)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
+// var express = require('express');
+// var router = express.Router();
+
+// // Authorize users.
+// router.get("/", function (req, res) {
+//   res.redirect("/users/login");
+// });
+
+
+// module.exports = router;
+
+
+// routes/index.js
 var express = require('express');
 var router = express.Router();
 
-// Authorize users.
 router.get("/", function (req, res) {
-  res.redirect("/users/login");
+  // Check session directly in the route
+  if (req.session && req.session.user) {
+    // Redirect to your main app (e.g., iframehost or webquiz)
+    return res.redirect("/iframehost"); 
+  } else {
+    // Only redirect to login if NOT authenticated
+    return res.redirect("/users/login");
+  }
 });
-
 
 module.exports = router;
